16048730c951a23310d65e832e27004b76aafe39
Giarcr0b <1109847@rgu.ac.uk>
2015-10-19 15:13:00 +0100

    choice to buy

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <?php
             $myage = 25;
             $name = "Bob";
+            $wantedgood = "mugs";
 
             if ($myage<16) {
                 print $name . " cant buy anything.";
@@ -19,6 +20,22 @@
             }
             else {
                 print $name . " can buy sausage rolls.";
+            }
+
+            switch ($wantedgood) {
+
+                case "specs":
+                    echo "You have to over 16 to buy " . $wantedgood;
+                    break;
+                case "mugs":
+                    echo "You have to be over 18 to buy " . $wantedgood;
+                    break;
+                case "sausage roll":
+                    echo "you have to be over 21 to buy " . $wantedgood;
+                    break;
+                default:
+                    echo "you cant buy " .$wantedgood . " in Aberdeen";
+
             }
           ?>
 </p>
