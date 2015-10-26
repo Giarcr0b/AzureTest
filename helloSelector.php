fbfad4de4140165b0defccaf9a6ddd27d343ea2f
Giarcr0b <1109847@rgu.ac.uk>
2015-10-26 15:36:25 +0000

    choice to buy

---
+++ helloSelector.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<h1>Where would you like to say Hello?</h1>
+<p>
+    <a href="helloPrinter.php?query=earth">Earth</a>
+</p>
+<p>
+    <a href="helloPrinter.php?query=mars">Mars</a>
+</p>
+<p>
+    <a href="helloPrinter.php?query=uranus">Uranus</a>
+</p>
+</body>
+</html>
