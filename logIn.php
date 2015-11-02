dfd2315feccad1f528b87ec2a3689c4ffaf76ae9
Giarcr0b <1109847@rgu.ac.uk>
2015-11-02 13:38:17 +0000

    Log in page

---
+++ logIn.php
@@ -0,0 +1,19 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<p>
+<h1>LOG IN PAGE</h1>
+</p>
+<p>
+<form action="loggedOn.php" method="post">
+    <label>User Name: </label><input type="text" name="name">
+    <label>Password: </label><input type="text" name="pass">
+    <input type="submit" value="Log On">
+
+</form>
+</p>
+</body>
+</html>
