dd81be2d841004878c937b7f892604cc7c9a73f2
Giarcr0b <1109847@rgu.ac.uk>
2015-11-02 15:32:24 +0000

    Log in page

--- logIn.php
+++ logIn.php
@@ -8,7 +8,7 @@
 <h1>LOG IN PAGE</h1>
 </p>
 <p>
-<form action=="loginCheck.php" method="post">
+<form action=="logInCheck.php" method="post">
     <label>User Name: </label><input type="text" name="name">
     <label>Password: </label><input type="text" name="pass">
     <input type="submit" value="Log On">
