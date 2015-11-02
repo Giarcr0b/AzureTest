54bb0f0657945c1a18c492a378e04b675d7d5277
Craig <1109847@rgu.ac.uk>
2015-11-02 22:07:28 +0000

    Log in again

--- homepage.php
+++ homepage.php
@@ -5,11 +5,11 @@
  * Date: 02/11/2015
  * Time: 13:44
  */
-if (isset($_COOKIE["Name"])) {
 
+    echo "hello";
     echo "Your Name is " . $_COOKIE["Name"];
     echo "Access level is " . $_COOKIE["access_level"];
 
-}
+
 
 ?>
