afe970a9878ed0d8190c5ae18ddb4ece71d86ea7
Giarcr0b <1109847@rgu.ac.uk>
2015-11-02 15:16:28 +0000

    Log in page

--- homepage.php
+++ homepage.php
@@ -5,4 +5,11 @@
  * Date: 02/11/2015
  * Time: 13:44
  */
+if (isset($_COOKIE["Name"])) {
 
+    echo "Your Name is " . $_COOKIE["Name"];
+    echo "Access level is " . $_COOKIE["access_level"];
+
+}
+
+?>
