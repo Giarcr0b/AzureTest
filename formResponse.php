325c08332318e9835c07f6e7dd635954108a5b91
Giarcr0b <1109847@rgu.ac.uk>
2015-11-09 15:29:23 +0000

    bootstrap website

--- formResponse.php
+++ formResponse.php
@@ -5,10 +5,17 @@
  * Date: 26/10/2015
  * Time: 15:46
  */
-$name = $_POST["forename"];
-$surname = $_POST["surname"];
+$name = $_POST["name"];
+$dob = $_POST["dob"];
+$gender = $_POST["gender"];
+$power1 = $_POST["power1"];
+$power2 = $_POST["power2"];
+$power3 = $_POST["power3"];
 
-echo "Name: " . $name . " Surname: " . $surname;
+
+echo "Name: $name  Date of Birth: $dob<br>";
+echo "Gender: $gender<br>";
+echo "$power1"
 
 ?>
 
