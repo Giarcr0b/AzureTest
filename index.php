f06906b94e9dff50b6f865ff4a402db699cdcb5c
Craig <1109847@rgu.ac.uk>
2015-12-09 20:14:11 +0000

    home testing

--- index.php
+++ index.php
@@ -81,16 +81,38 @@
                     print "no products are available";
                 }
             }
+
+    $db = new mysqli("eu-cdbr-azure-north-d.cloudapp.net", "b3db0ca6efa7a5", "eb1ce59a", "DataBase_1109847");
+
+    if ($db->connect_errno) {
+        die('Connectfailed[' . $db->connect_error . ']');
+    }
+
+    $sql_query = "SELECT * FROM marvelmovies WHERE productionStudio = '$query'";
+
+    $result = $db->query($sql_query);
+echo "<h1>Which Studios Super Hero Films do you want to display?</h1>";
+
+    while($row = $result->fetch_array()){
+        echo "<p>".
+    "<a href="."'superHero.php?query='".$row['productionStudio'].">".$row['productionStudio']."</a>".
+"</p>";
+    }
+
+    $result->close();
+
+    $db->close();
           ?>
+
 
 </p>
 <p>
     <a href="helloSelector.php">Hello World</a>
 </p>
 
-<h1>Which Studios Super Hero Films do you want to display?</h1>
+
 <p>
-    <a href="superHero.php?query=Universal Studios">Universal Studios</a>
+    "<a href=""superHero.php?query=Universal Studios">$row['productionStudio']"</a>"
 </p>
 <p>
     <a href="superHero.php?query=20th Century Fox">20th Century Fox</a>
