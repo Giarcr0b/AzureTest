dbcff1a2088d360d14866e1f8a817eceee0402e5
Craig <1109847@rgu.ac.uk>
2015-12-09 12:23:09 +0000

    home testing

--- superHero.php
+++ superHero.php
@@ -11,7 +11,7 @@ $db = new mysqli("eu-cdbr-azure-north-d.cloudapp.net", "b3db0ca6efa7a5", "eb1ce5
 if ($db->connect_errno) {
     die('Connectfailed[' . $db->connect_error . ']');
 }
-
+echo "HEllo";
 
 ?>
 
