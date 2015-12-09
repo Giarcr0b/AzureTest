c18c7fe17cd8b5df4e5ecdbebde8b046827e25ab
Craig <1109847@rgu.ac.uk>
2015-12-09 11:56:23 +0000

    home testing

--- superHero.php
+++ superHero.php
@@ -16,17 +16,17 @@ $db = new mysqli("eu-cdbr-azure-north-d.cloudapp.net", "b3db0ca6efa7a5", "eb1ce5
 if ($db->connect_errno) {
     die('Connectfailed[' . $db->connect_error . ']');
 }
-//    $sql_query = "SELECT * FROM marvelmovies WHERE productionStudio LIKE '%Fox%'";
-//
-//    $result = $db->querry($sql_query);
-//
-//    while($row = $result->fetch_array()){
-//        echo "<p>" . $row['title'] . "</p>";
-//    }
-//
-//$result->close();
-//
-//$db->close();
+    $sql_query = "SELECT * FROM marvelmovies WHERE productionStudio LIKE '%Fox%'";
+
+    $result = $db->querry($sql_query);
+
+    while($row = $result->fetch_array()){
+        echo "<p>" . $row['title'] . "</p>";
+    }
+
+$result->close();
+
+$db->close();
 
 
 ?>
